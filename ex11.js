15bb0866257b193dc9863f9eb962062c265db44c
Ayaka Ito <a.8.32.noko.info@gmail.com>
2024-10-01 12:23:09 -0700

    exercise 11 completed

--- ex11.js
+++ ex11.js
@@ -2,4 +2,20 @@
 // and returns the number of times the word appears in the string.
 // Example: countWordOccurrences("hello world hello", "hello") should return 2.
 
+function countWordOccurrences(message, word) {
+  const messageArr = message.split(/\s/);
+  let count = {};
+  for (let i = 0; i < messageArr.length; i++) {
+    if (count[messageArr[i]]) {
+      count[messageArr[i]]++;
+    } else {
+      count[messageArr[i]] = [];
+      count[messageArr[i]] = 1;
+    }
+  }
+
+  return count[word];
+}
+
 console.log(countWordOccurrences("hello world hello", "hello")); // Expected output: 2
+console.log(countWordOccurrences("hello world hello hello hello hello", "world")); // Expected output: 1
